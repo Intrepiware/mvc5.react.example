6969f72089cca5add2f353a21c397ed05564b6ca
Mike Devlin <mike@epay3.com>
2022-05-20 08:22:11 -0500

    Fix build; make greeting time-sensitive

--- MVC5.React.Example/Scripts/src/home-contact.js
+++ MVC5.React.Example/Scripts/src/home-contact.js
@@ -3,10 +3,22 @@ import ReactDOM from 'react-dom';
 import Shared from './Components/shared';
  
 class ContactComponent extends Component {
+
+    getHourText() {
+        const hour = new Date().getHours();
+        if(hour < 12)
+            return "Good morning";
+        if(hour < 5)
+            return "Good afternoon";
+        if(hour < 9)
+            return "Good evening";
+        else
+            return "Good night";
+    }
     render() {
         return(
             <div>
-                <h1>Good night from Contact Component!</h1>
+                <h1>{this.getHourText()} from Contact Component!</h1>
                 <Shared message="I have been placed here from the Home/Contact page"></Shared>
             </div>
         );
